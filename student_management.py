61dc45af80dc7c96e5e0d92f919448e0de19545f
sushmitha-programmer <sushmithag437@gmail.com>
2026-06-30 07:51:32 +0530

    Create student_management.py

---
+++ student_management.py
@@ -0,0 +1,17 @@
+# Student Management System
+
+This is my first Python project.
+
+## Features
+- Add Student
+- View Students
+- Search Student
+- Update Marks
+- Delete Student
+- Calculate Average Marks
+
+## Language
+- Python
+
+## Author
+Sushmitha G
